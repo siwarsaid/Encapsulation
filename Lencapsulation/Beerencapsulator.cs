caa5eb927a0ef3fc948c10159d6f633cc57b7633
siwar said <siwarsaid8@gmail.com>
2024-04-16 23:54:23 +0200

    quete encapsulation

---
+++ Lencapsulation/Beerencapsulator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lencapsulation
+{
+    internal class Beerencapsulator
+    {
+        private double _avalaibleBeerVolume;
+        private int _avalaibleBottles;
+        private int _avalaibleCapsules;
+        public Beerencapsulator()
+        {           
+            
+        }
+        public double GetAvalaibleBeerVolume()
+        {
+            return _avalaibleBeerVolume;
+        }
+        public int GetAvalaibleBottles()
+        {
+            return _avalaibleBottles;
+        }
+        public int GetAvalaibleCapsules()
+        {
+            return _avalaibleCapsules;
+        }
+        public void SetAvalaibleBottles(int bottles)
+        {
+            _avalaibleBottles = bottles;
+        }
+        public void SetAvalaibleCapsules(int capsules)
+        {
+            _avalaibleCapsules = capsules;
+        }
+        public void AddBeer(double quantite)
+        {
+
+
+            if (quantite > 0)
+            {
+                _avalaibleBeerVolume += quantite;
+            }
+            else
+            {
+                Console.WriteLine("Quantité de bière ajoutée invalide!!  ");
+            }
+            
+
+        }
+        public int ProduceEncapsulatedBeerBottles(int nbBottles)
+        {
+           if (nbBottles <= 0)
+            {
+                Console.WriteLine(" Nombre de bouteilles non valide !!");
+                return 0;
+            }
+
+             var bouteillesPossibles = _avalaibleBeerVolume / 33;
+           
+
+            if( bouteillesPossibles>= nbBottles && _avalaibleCapsules>=nbBottles)
+            {
+                _avalaibleBottles -= nbBottles;
+                _avalaibleCapsules -= nbBottles;
+                _avalaibleBeerVolume -= nbBottles * 33;
+                return nbBottles;
+            }
+            if(bouteillesPossibles==0 || _avalaibleCapsules==0 )
+            {
+                return 0;
+            }
+             int res = (int)Math.Min(bouteillesPossibles, _avalaibleCapsules);
+            _avalaibleBottles -= (int)res;
+            _avalaibleCapsules -= (int)res;
+            _avalaibleBeerVolume = (int)res;
+            return (int)res;
+
+            }
+            
+        }
+    }
+ 
+
---
+++ Lencapsulation/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Lencapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Beerencapsulator machine = new Beerencapsulator();
+
+            Console.Write("Saisie le volume de bière: ");
+            double volumeBeer = double.Parse(Console.ReadLine()+"\n");
+            while (volumeBeer <= 0)
+            {
+                Console.WriteLine("Quantité de bière ajoutée invalide!!  \n");
+                Console.Write("Saisie le volume de bière: ");
+                volumeBeer = double.Parse(Console.ReadLine() + "\n");
+            }
+            machine.AddBeer(volumeBeer);
+
+            Console.Write("Saisie les bouteilles vides: ");
+            int bouteilles = Int32.Parse(Console.ReadLine() + "\n");
+            while (bouteilles < 0)
+            {
+                Console.WriteLine("Le nombre de bouteilles ne peut pas être négatif !!");
+                Console.Write("Saisie les bouteilles vides: ");
+                bouteilles = Int32.Parse(Console.ReadLine() + "\n");
+            }
+            machine.SetAvalaibleBottles(bouteilles);
+
+            Console.Write("Saisie le nombre des capsules : ");
+             int capsules = Int32.Parse(Console.ReadLine() + "\n");
+
+             while (capsules < 0) 
+              {
+                Console.Write("Le nombre de capsules ne peut pas être négatif!! "); 
+              
+                Console.Write("Saisie le nombre des capsules : \n");
+                capsules = Int32.Parse(Console.ReadLine() + "\n");
+              }
+
+
+                machine.SetAvalaibleCapsules(capsules);
+            Console.Write("Saisie le nombre des bouteilles a produire: ");
+            int boteillesaProduire = Int32.Parse(Console.ReadLine() + "\n");
+
+            Console.WriteLine( machine.ProduceEncapsulatedBeerBottles(boteillesaProduire) + " bouteilles sont produites \n");
+
+            Console.WriteLine(" Volume de bière restant : " + machine.GetAvalaibleBeerVolume() + " cl\n");
+            Console.WriteLine(machine.GetAvalaibleBottles() + " bouteilles restantes \n");
+            Console.WriteLine(machine.GetAvalaibleCapsules() + " capsules restantes \n");
+
+           
+        }
+
+    }
+}
